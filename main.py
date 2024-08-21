f16ba899240dfd9b8a63436389c4917cf10e2062
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-08-21 12:10:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 url="bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
-print(url)
 
-url_base = url[0:19]
-print(url_base)
+indice_fatiamento = url.find('?')
+url_base = url[0:indice_fatiamento]
+
+
+url_parametros = url[indice_fatiamento+1:]
+print(url_parametros)
+
+pametros_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(pametros_busca)
+
+print(indice_fatiamento)
+
+indice_valor = 
